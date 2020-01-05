0346061d52ef8408338afd74983c92bb5675671d
bondalex-maker <bondarenko.alex.sergeevich@gmail.com>
2020-01-05 20:14:23 +0200

    create html css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import styles from './assets/sass/style.scss';
+
+(function() {
+    /*FIXED HEADER*/
+    let header = document.getElementById("header");
+    let intro = document.getElementById("intro");
+    let introH;
+    let scrollPos = window.scrollY;
+
+    window.addEventListener("scroll", function (e) {
+        introH = intro.offsetHeight;
+        scrollPos = this.scrollY;
+        if (scrollPos > introH){
+            header.classList.add("fixed");
+
+
+        } else {
+            header.classList.remove("fixed");
+
+
+        }
+
+    });
+        /*Nav toggle*/
+    let nav = document.getElementById('nav');
+    nav.classList.add('nav');
+    let navToggle = document.getElementById('navToggle');
+    navToggle.onclick = function (e) {
+        e.preventDefault();
+        nav.classList.add('show');
+
+    }
+
+})();
