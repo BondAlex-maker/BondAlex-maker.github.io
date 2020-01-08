0a1d51871874ef1fb31eee70909654da39c091d4
bondalex-maker <bondarenko.alex.sergeevich@gmail.com>
2020-01-08 15:47:27 +0200

    add burger button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import styles from './assets/sass/style.scss';
 
+'use strict';
+
 (function() {
     /*FIXED HEADER*/
     let header = document.getElementById("header");
@@ -21,14 +23,52 @@ import styles from './assets/sass/style.scss';
         }
 
     });
-        /*Nav toggle*/
-    let nav = document.getElementById('nav');
-    nav.classList.add('nav');
-    let navToggle = document.getElementById('navToggle');
-    navToggle.onclick = function (e) {
-        e.preventDefault();
-        nav.classList.add('show');
 
+    /*Nav Toggle*/
+    let nav = document.getElementById('nav');
+    let navToggle = document.getElementById('navToggle');
+    navToggle.onclick = function() {
+        nav.classList.toggle('show');
     }
 
+
 })();
+
+
+
+/*Slider
+========= ============*/
+
+const slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+const slideInterval = setInterval(nextSlide,5000);
+
+function secondSlide() {
+    goToSlide(1);
+}
+
+function firstSlide() {
+    goToSlide(0);
+}
+
+function nextSlide () {
+    goToSlide(currentSlide+1);
+}
+
+function goToSlide(n) {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+/*NEXT PREV*/
+
+const next = document.getElementById('second');
+const previous = document.getElementById('first');
+
+next.onclick = function() {
+    secondSlide();
+};
+previous.onclick = function() {
+    firstSlide();
+};
